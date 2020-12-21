a6cce9f37631f0f06f11fbd93fccf83e840cc74d
Bartolomeuss <74045308+Bartolomeuss@users.noreply.github.com>
2020-12-21 15:51:50 +0100

    addition of the excisting files

---
+++ index.js
@@ -0,0 +1,164 @@
+
+let player1Score = 0
+let player2Score = 0
+let player1Turn = true
+let penalty = -3;
+let beginner = 1
+
+const player1Dice = document.getElementById("player1Dice")
+const player2Dice = document.getElementById("player2Dice")
+const player1Scoreboard = document.getElementById("player1Scoreboard")
+const player2Scoreboard = document.getElementById("player2Scoreboard")
+const message = document.getElementById("message")
+const rollBtn = document.getElementById("rollBtn")
+const resetBtn = document.getElementById("resetBtn")
+const dubBtn = document.getElementById("dubBtn")
+const listWinner = document.createElement('LI')
+const player1win = document.createTextNode("Player 1")
+const player2win = document.createTextNode("Player 2")
+const list = document.getElementById('winner-list')
+
+function showResetButton() {
+    rollBtn.style.display = "none"
+    dubBtn.style.display = "none"
+    resetBtn.style.display = "block"
+}
+
+ rollBtn.addEventListener("click", function() {
+    const randomNumber = Math.floor(Math.random() * 6) + 1
+
+    if (player1Turn) {
+        player1Score += randomNumber 
+        player1Scoreboard.textContent = player1Score
+        player1Dice.textContent = randomNumber
+        player1Dice.classList.remove("active")
+        player2Dice.classList.add("active")
+        message.textContent = "Player 2 Turn"
+    } else {
+        player2Score += randomNumber
+        player2Scoreboard.textContent = player2Score
+        player2Dice.textContent = randomNumber
+        player2Dice.classList.remove("active")
+        player1Dice.classList.add("active")
+        message.textContent = "Player 1 Turn"
+    }
+    
+    if (player1Score >= 20) {
+        message.textContent = "Player 1 Wins"
+        showResetButton()
+        
+        list.innerText=""
+        listWinner.appendChild(player1win)
+        list.appendChild(player1win)
+
+       
+
+    }  else if (player2Score >= 20) {
+        message.textContent = "Player 2 Wins"
+        showResetButton()
+        
+        list.innerText=""
+        listWinner.appendChild(player2win)
+        list.appendChild(player2win)
+       
+      
+        
+    }
+    player1Turn = !player1Turn
+})
+
+dubBtn.addEventListener("click", function() {
+    let randomNumber = Math.floor(Math.random() * 12) + 2
+
+    if(randomNumber > 9){
+       
+        randomNumber = penalty;
+        penalty -= 3;
+    }
+    
+    
+     if (player1Turn) {
+        player1Score += randomNumber 
+        player1Scoreboard.textContent = player1Score
+        player1Dice.textContent = randomNumber
+        player1Dice.classList.remove("active")
+        player2Dice.classList.add("active")
+        message.textContent = "Player 2 Turn"
+    } else {
+        player2Score += randomNumber
+        player2Scoreboard.textContent = player2Score
+        player2Dice.textContent = randomNumber
+        player2Dice.classList.remove("active")
+        player1Dice.classList.add("active")
+        message.textContent = "Player 1 Turn"
+    }
+    
+    if (player1Score >= 20) {
+        message.textContent = "Player 1 Wins"
+        showResetButton()
+        
+        list.innerText=""
+        listWinner.appendChild(player1win)
+        list.appendChild(player1win)
+
+       
+
+    }  else if (player2Score >= 20) {
+        message.textContent = "Player 2 Wins"
+        showResetButton()
+        
+        list.innerText=""
+        listWinner.appendChild(player2win)
+        list.appendChild(player2win)
+       
+      
+        
+    }
+    player1Turn = !player1Turn
+})
+
+ 
+resetBtn.addEventListener("click", function(){
+    reset()
+})
+
+function playerBegins(){
+    if (beginner % 2 == 0){
+        player1Turn = false
+        player1Dice.classList.remove("active")
+        player2Dice.classList.add("active")
+        message.textContent = "Player 2 Turn"
+    }else {
+        player1Turn = true
+        player2Dice.classList.remove("active")
+        player1Dice.classList.add("active")
+        message.textContent = "Player 1 Turn"
+    }}
+
+
+
+
+
+
+
+function reset() {
+    player1Score = 0
+    player2Score = 0
+   
+   player1Scoreboard.textContent = 0
+    player2Scoreboard.textContent = 0
+    player1Dice.textContent = "-"
+    player2Dice.textContent = "-"
+    
+    
+    message.textContent = "Player 1 Turn"
+    resetBtn.style.display = "none"
+    rollBtn.style.display = "block"
+    dubBtn.style.display = "block"
+    penalty = -3;
+ 
+    beginner ++;
+    playerBegins()
+    
+
+}
